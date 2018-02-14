ace44aedf33f2a2aa1b4b0408950c9cbd7250f17
manoj <manoj@Admins-MacBook-Air.local>
2018-02-15 00:29:11 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
-     
+
 }
 
 
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+
 
 ]
